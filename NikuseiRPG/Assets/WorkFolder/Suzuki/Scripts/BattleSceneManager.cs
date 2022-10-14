eae23dac83fa76a3fb380c3cb791b46c912cc34f
DESKTOP-DUMTH60\MAX-C <Sourcetree>
2022-10-15 02:59:20 +0900

    エネミー途中

--- NikuseiRPG/Assets/WorkFolder/Suzuki/Scripts/BattleSceneManager.cs
+++ NikuseiRPG/Assets/WorkFolder/Suzuki/Scripts/BattleSceneManager.cs
@@ -19,13 +19,79 @@ public class BattleSceneManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    [SerializeField,Tooltip("バトルの進行管理用のテキスト")]
-    Text battleText;
+    [SerializeField, Tooltip("バトルの進行管理用のテキスト")]
+    private Text battleText;
     [SerializeField, Tooltip("EnemyListを入れる")]
-    EnemyList enemyList;
+    private EnemyList enemyList;
+    [Tooltip("戦闘中かどうかを判定")]
+    private bool battlePhase = true;
+    [Tooltip("勝敗判定")]
+    private bool winOrLose = true;
+    /*
+    [Tooltip("敵の番号を受け取る")]
+    private int enemyNumber;
+    */
+
+    //--仮置きのプレイヤーのステータス--
+    [SerializeField]
+    int np = 3;
+    // int atk = 3;
+    //int agi = 2;
+    //  int def = 0;
+    //--------------------------------
+    // 敵のステータス
+    private string enemyName;
+    private int enemyNp;
+    private int enemyAtk;
+    private int enemyAgi;
+    private int enemyDef;
+    private int enemyLv;
+
     public void Start()
     {
+        // 敵の情報を設定
+        //                                   ↓ここを後でenemyNumberに変える多分
+        enemyName = enemyList.EnemyParamList[0].enemyName;
+        enemyNp = enemyList.EnemyParamList[0].np;
+        enemyAtk = enemyList.EnemyParamList[0].atk;
+        enemyAgi = enemyList.EnemyParamList[0].agi;
+        enemyDef = enemyList.EnemyParamList[0].def;
+        enemyLv = enemyList.EnemyParamList[0].lv;
+
         // 遭遇した敵の情報をEnemyListから貰い、敵の名前をテキストに描画
-        battleText.text = enemyList.EnemyParamList[0].enemyName;
+     　 battleText.text = enemyName + "に遭遇した！！";
+    }
+    public void Update()
+    {
+        
+        // 戦闘開始
+        if (battlePhase)
+        {
+            // 戦闘終了判定
+            if (enemyNp <= 0)
+            {
+                battlePhase = false;
+            }
+            else if (np <= 0)
+            {
+                battlePhase = false;
+                // どちらかが勝ったかの判定
+                winOrLose = false;
+            }
+        }
+        else
+        {
+            // プレイヤーの勝利
+            if(winOrLose)
+            {
+                battleText.text = enemyName + "を倒した！";
+            }
+            // プレイヤーの敗北
+            else
+            {
+                battleText.text = "GAMEOBERA";
+            }
+        }
     }
 }
+//battleText.text = string.Format("{000}",enemyNp);
